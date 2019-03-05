3dc45140393ee6aee9020c91fae6296737fdb352
Mikko Maja <mikmaj88@gmail.com>
2019-03-05 19:10:15 +0200

    Add django-heroku

--- django_webstore/settings.py
+++ django_webstore/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -133,3 +134,5 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
