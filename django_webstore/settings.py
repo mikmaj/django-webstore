61fe071d61bbcd49154f0a0a077171872abc4886
Mikko Maja <mikmaj88@gmail.com>
2019-03-05 19:37:10 +0200

    Set debug value

--- django_webstore/settings.py
+++ django_webstore/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJ_WEBSTORE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DJ_WEBSTORE_DEBUG') == 'True')
 
 ALLOWED_HOSTS = ['mikmaj-djangowebstore.herokuapp.com']
 
